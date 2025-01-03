4905bf701da5afd445e657e47e1e847fd261908e
Gil Alpert <alpertgil@gmail.com>
2025-01-03 17:35:39 +0200

    DynamicLibrary class to load dlls and call exported functions templately

---
+++ Common/src/DynamicLibrary.cpp
@@ -0,0 +1,34 @@
+﻿#include "Include/DynamicLibrary.hpp"
+
+#include "Include/Exception.hpp"
+#include "Include/Trace.hpp"
+
+DynamicLibrary::DynamicLibrary(const std::filesystem::path& dll_path):
+	m_module(load_library(dll_path))
+{
+}
+
+DynamicLibrary::~DynamicLibrary()
+{
+	try
+	{
+		if (FreeLibrary(m_module) == FALSE)
+		{
+			TRACE(L"failed to free library")
+		}
+	}
+	catch (...)
+	{
+		TRACE(L"failed to free library")
+	}
+}
+
+HMODULE DynamicLibrary::load_library(const std::filesystem::path& dll_path)
+{
+	const HMODULE result = LoadLibraryW(dll_path.wstring().c_str());
+	if (result == nullptr)
+	{
+		throw WinApiException(ErrorCode::FAILED_LIBRARY_LOAD);
+	}
+	return result;
+}
---
+++ Common/src/Include/DynamicLibrary.hpp
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "Exception.hpp"
+
+#include <filesystem>
+#include <functional>
+#include <Windows.h>
+
+class DynamicLibrary final
+{
+public:
+	explicit DynamicLibrary(const std::filesystem::path& dll_path);
+	~DynamicLibrary();
+	DynamicLibrary(const DynamicLibrary&) = delete;
+	DynamicLibrary& operator=(const DynamicLibrary&) = delete;
+	DynamicLibrary(DynamicLibrary&&) = delete;
+	DynamicLibrary& operator=(DynamicLibrary&&) = delete;
+
+private:
+	[[nodiscard]] static HMODULE load_library(const std::filesystem::path& dll_path);
+
+public:
+	template <typename FunctionType, typename... Args>
+	auto call(const std::string& name, Args&&... args) const
+	{
+		const FARPROC result = GetProcAddress(m_module, name.c_str());
+		if (result == nullptr)
+		{
+			throw WinApiException(ErrorCode::FAILED_LIBRARY_GET_PROC_ADDRESS);
+		}
+		auto func = reinterpret_cast<FunctionType>(result);
+		return (*func)(std::forward<Args>(args)...);
+	}
+
+private:
+	HMODULE m_module;
+};
